809a1b7937bfcc83ef307d86de1d522f7bdb06d0
Alice <alicegillbanks@gmail.com>
2026-01-16 20:52:22 +0000

    BetterRest
    Project 4! Uses Core ML to estimate when someone should go to bed

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michael Gillbanks on 14/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
