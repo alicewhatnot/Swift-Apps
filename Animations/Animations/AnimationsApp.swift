7f1b72a30f5216a42c7202cefee37fd989d7e39b
Alice <alicegillbanks@gmail.com>
2026-01-22 22:00:24 +0000

    Started Animation Project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Michael Gillbanks on 22/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
