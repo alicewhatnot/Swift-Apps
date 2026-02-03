085abd459aa4146c793aa7a5dd60e6051ac97896
Alice <alicegillbanks@gmail.com>
2026-02-03 20:35:21 +0000

    Created iExpense

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Michael Gillbanks on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
