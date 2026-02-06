bbd68e079edbe658ea805ffe1b6fda874ae9444c
Alice <alicegillbanks@gmail.com>
2026-02-06 19:27:19 +0000

    Learning Paths

---
+++ HackingWithSwift/P9-Navigation/P9-Navigation/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  P9-Navigation
+//
+//  Created by Michael Gillbanks on 06/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var path = NavigationPath()
+    
+    var body: some View {
+        NavigationStack(path: $path) {
+            List {
+                ForEach(1..<5) { number in
+                    NavigationLink("select number \(number)", value: number)
+                }
+                ForEach(1..<5) { number in
+                    NavigationLink("select string \(number)", value: String(number))
+                }
+                
+            }
+            .toolbar {
+                Button("556") {
+                    path.append(556)
+                }
+                
+                Button("hello") {
+                    path.append("hello")
+                }
+            }
+            .navigationDestination(for: Int.self) { _ in
+                Text("int")
+            }
+            .navigationDestination(for: String.self) { _ in
+                Text("string")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
