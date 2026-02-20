606ee2b0cca14bc68e004b8fb22eb5022455156f
Alice <alicegillbanks@gmail.com>
2026-02-20 17:40:37 +0000

    Bit more swift data practice before the challenge

--- HackingWithSwift/M4-SwiftDataProject/M4-SwiftDataProject/User.swift
+++ HackingWithSwift/M4-SwiftDataProject/M4-SwiftDataProject/User.swift
@@ -21,3 +21,39 @@ class User {
         self.joinDate = joinDate
     }
 }
+
+let str = """
+    {
+        "first": "Andrew",
+        "last": "Glouberman",
+        "age": "13"
+    }
+    """
+
+struct Test: Codable {
+    enum CodingKeys: String, CodingKey {
+        case firstName = "first"
+        case lastName = "last"
+        case age
+    }
+    
+    var firstName: String
+    var lastName: String
+    var age: Int
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.firstName = try container.decode(String.self, forKey: .firstName)
+        self.lastName = try container.decode(String.self, forKey: .lastName)
+        var stringAge = try container.decode(String.self, forKey: .age)
+        self.age = Int(stringAge) ?? 0
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.firstName, forKey: .firstName)
+        try container.encode(self.lastName, forKey: .lastName)
+        try container.encode(String(self.age), forKey: .age)
+    }
+}
+    
