419f3e4526192b28ad4f6462cba02e800740ae22
Alice <alicegillbanks@gmail.com>
2026-02-23 22:35:43 +0000

    Started BucketList

---
+++ HackingWithSwift/P14-BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Michael Gillbanks on 23/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/P14-BucketList/BucketList/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Michael Gillbanks on 23/02/2026.
+//
+
+import MapKit
+import SwiftUI
+
+struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 55, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        ))
+        
+    @State private var locations = [Location]()
+    @State private var selectedPlace: Location?
+    
+    var body: some View {
+        MapReader { proxy in
+            Map(initialPosition: startPosition) {
+                ForEach(locations) { location in
+                    Annotation(location.name, coordinate: location.coordinate) {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundStyle(.red)
+                            .frame(width: 44, height: 44)
+                            .background(.white)
+                            .clipShape(.circle)
+                            .onLongPressGesture(minimumDuration: 0.1) {
+                                selectedPlace = location
+                            }
+                    }
+                }
+            }
+                .mapStyle(.hybrid)
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        locations.append(newLocation)
+                    }
+                }
+                .sheet(item: $selectedPlace) { place in
+                    EditView(location: place) { newLocation in
+                        if let index = locations.firstIndex(of: place) {
+                            locations[index] = newLocation
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
