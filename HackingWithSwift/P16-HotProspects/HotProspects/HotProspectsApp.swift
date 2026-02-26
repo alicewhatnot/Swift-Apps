ca6af14ccf0230f2b57cb85608b8da35a3e21a80
Alice <alicegillbanks@gmail.com>
2026-02-26 21:36:42 +0000

    Started work on HotProspects

---
+++ HackingWithSwift/P16-HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Michael Gillbanks on 26/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
