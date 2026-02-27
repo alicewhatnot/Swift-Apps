8eeacca3fbd1c4de46c6b6574d976606c38a4b4c
Alice <alicegillbanks@gmail.com>
2026-02-27 13:39:51 +0000

    Day 82 - Started main program

--- HackingWithSwift/P16-HotProspects/HotProspects/HotProspectsApp.swift
+++ HackingWithSwift/P16-HotProspects/HotProspects/HotProspectsApp.swift
@@ -5,6 +5,7 @@
 //  Created by Michael Gillbanks on 26/02/2026.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct HotProspectsApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Prospect.self)
     }
 }
