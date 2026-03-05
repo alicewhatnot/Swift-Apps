9906973fb0ac8d3debdc3d264ceee57a1754d9b4
Alice <alicegillbanks@gmail.com>
2026-03-05 22:00:55 +0000

    Started SnowSeeker! Final proj!

---
+++ HackingWithSwift/P19-SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Michael Gillbanks on 05/03/2026.
+//
+
+import SwiftUI
+
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct HighScoreView: View {
+    @Environment(Player.self) var player
+
+    var body: some View {
+        @Bindable var player = player
+        Stepper("High score: \(player.highScore)", value: $player.highScore)
+    }
+}
+
+struct ContentView: View {
+    @State private var player = Player()
+
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+            HighScoreView()
+        }
+        .environment(player)
+    }
+}
+
+#Preview {
+    ContentView()
+}
